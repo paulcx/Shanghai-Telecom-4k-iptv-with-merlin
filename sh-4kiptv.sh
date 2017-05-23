ab85e5ae3143aa138f6ada275d2775acc31c1d8d
fanny.wu <love19862003@163.com>
2017-05-23 23:23:09 +0800

    Update sh-4kiptv.sh

--- sh-4kiptv.sh
+++ sh-4kiptv.sh
@@ -28,7 +28,7 @@ echo 正在删除旧文件
 rm -rf 4k-start*
 #下载开机运行脚本
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/4k-start
+wget -q --no-check-certificate https://raw.githubusercontent.com/love19862003/Shanghai-Telecom-4k-iptv-with-merlin/master/4k-start
 #设置权限
 chmod -R 0755 4k-start
 #完成提示
@@ -42,13 +42,13 @@ echo
 #脚本提示
 echo 正在修改ss dnsmasq配置文件
 #移动到ss dnsmasq目录
-cd /jffs/ss/redchn
+cd /jffs/ss/rules
 #删除旧配置文件
 echo 正在删除旧文件
 rm -rf dnsmasq.postconf
 #下载新配置文件
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
+wget -q --no-check-certificate https://raw.githubusercontent.com/love19862003/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
 #重命名新配置文件
 mv ss dnsmasq.postconf
 #设置权限
@@ -70,7 +70,7 @@ echo 正在删除旧文件
 rm -rf dnsmasq.postconf
 #下载新配置文件
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
+wget -q --no-check-certificate https://raw.githubusercontent.com/love19862003/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
 #重命名新配置文件
 mv noss dnsmasq.postconf
 #设置权限
